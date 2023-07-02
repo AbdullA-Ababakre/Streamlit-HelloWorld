1fa2037687584a553ca51b06b45ac7e5274c1501
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-07-02 11:15:42 +0800

    new app for conversational

---
+++ conversation_app.py
@@ -0,0 +1,34 @@
+# to use st.chat_message() and st.chat_input()
+# require streamlit Version 1.24.0
+# pip install streamlit --upgrade
+
+import streamlit as st
+
+st.title("Streamlit Conversation App")
+
+# this should have two containers (one for user and one for assistant)
+# and a input box for user
+
+# initialize the session state
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# echo the user input
+# := is used to assign a value to a variable in the same line as the variable is declared
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # add message to history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    with st.chat_message("assistant"):
+        st.markdown(response)
+
+    # add the echo message to chat history
+    st.session_state.messages.append({"role":"assistant","content":response})
