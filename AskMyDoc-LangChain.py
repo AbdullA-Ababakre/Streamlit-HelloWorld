afd70f08df49c1742f8266d883a0e516c93a4a50
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-07-13 13:46:55 +0800

    Update AskMyDoc-LangChain.py

--- AskMyDoc-LangChain.py
+++ AskMyDoc-LangChain.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from langchain llms import OPENAI
-from langchain textspilter import CharactorTextSpliter
+import openai
+from langchain.llms import OpenAI
+from langchain.text_splitter import CharactorTextSpliter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
@@ -11,5 +12,11 @@ st.set_page_config(page_title="LangChain Ask the Doc", page_icon="🦜🔗", lay
 st.title("LangChain Ask the Doc")
 
 uploaded_file = st.file_uploader("Upload a pdf file", type="pdf")
+
+st.write("This app needs OpenAI API key to work. Please enter your API key below \n\n the key should start with 'sk-'")
+# api_key should not be visible to the user, it should be like *****
+api_key = st.text_input("API Key", type="password")
+openai.api_key = api_key
+
 if uploaded_file is not None:
     st.write("File uploaded")
