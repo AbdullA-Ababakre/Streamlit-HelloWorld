4e82a8336f6544d73304a525026dbabfd71dea30
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-07-13 13:40:02 +0800

    Update AskMyDoc-LangChain.py

--- AskMyDoc-LangChain.py
+++ AskMyDoc-LangChain.py
@@ -7,3 +7,9 @@ from langchain.chains import RetrievalQA
 
 # Build an Ask the Doc app
 
+st.set_page_config(page_title="LangChain Ask the Doc", page_icon="🦜🔗", layout="wide")
+st.title("LangChain Ask the Doc")
+
+uploaded_file = st.file_uploader("Upload a pdf file", type="pdf")
+if uploaded_file is not None:
+    st.write("File uploaded")
