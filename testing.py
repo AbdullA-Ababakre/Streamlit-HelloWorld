9c1ccee01ab7483c631b2ead2c05f44b5917e01b
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-07-17 11:48:44 +0800

    test upload pdf function

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import base64 
+from pypdf import PdfReader
 # title of the app
 st.title("Streamlit new chat elements")
 
@@ -9,7 +10,16 @@ st.title("Streamlit new chat elements")
 # with st.chat_message("assistant"):
 #     st.write("Hello 👋 I'm assistant")
 
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 
-prompt = st.chat_input("Input here")
-if prompt:
-    st.markdown(f"Echo is working: {prompt}")
+def load_pdf_data(uploaded_file):
+    pdf = PdfReader(uploaded_file)
+    text = ""
+    for page in pdf.pages:
+        text += page.extract_text()
+    return text
+
+if uploaded_files is not None:
+    st.write("File uploaded")
+    for uploaded_file in uploaded_files:
+        st.markdown(load_pdf_data(uploaded_file))
