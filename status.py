081a2b9834ebabc5354939985675b87b40eeda10
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-09-03 15:50:05 +0800

    Create status.py

---
+++ status.py
@@ -0,0 +1,8 @@
+# in streamlit 1.26.0 version, st.status() is added
+# Insert a status container to display output from long-running tasks.
+
+import streamlit as st
+
+st.title('Streamlit new function Status')
+
+st.write('hello world')
