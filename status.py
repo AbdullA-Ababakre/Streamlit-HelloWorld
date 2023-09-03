fede58b725a9a8cc00c0a5f3f4201236c96f7970
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-09-03 16:14:18 +0800

    Update status.py

--- status.py
+++ status.py
@@ -2,7 +2,16 @@
 # Insert a status container to display output from long-running tasks.
 
 import streamlit as st
+import time
 
 st.title('Streamlit new function Status')
 
 st.write('hello world')
+
+with st.status("counting down", expanded=True) as status:
+    for i in range(10, 0, -1):
+        st.write(f"{i}...")
+        time.sleep(1)
+    status.update(label="Time is up", state="complete", expanded=False)
+
+st.button('Rerun')
