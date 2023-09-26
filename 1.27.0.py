5b15c174c2248f3d48ad865bf48db0dc9701190d
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-09-26 17:18:18 -0400

    Create 1.27.0.py

---
+++ 1.27.0.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+st.write(st.__version__)
+
+np.random.seed(42)
+#using st.altair_chart 
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+c = (
+    alt.Chart(chart_data)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.altair_chart(c, use_container_width=True)
+
+#using st.scatter_chart
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+#st.scatter_chart(chart_data)
