95c411e2add34624d882e3458075620cccd53593
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-11-01 22:34:39 -0400

    updated 1.28 file

--- version-1-28-0/test_streamlit_1_28.py
+++ version-1-28-0/test_streamlit_1_28.py
@@ -1,26 +1,54 @@
-import pytest
-from streamlit_1_28 import Wallet, InsufficientAmount
+import unittest
+import openai
+from unittest.mock import patch
+from types import SimpleNamespace
+from streamlit.testing.v1 import AppTest
 
+class TestChatbotApp(unittest.TestCase):
 
-def test_default_initial_amount():
-    wallet = Wallet()
-    assert wallet.balance == 0
+    def test_smoke(self):
+        """Test if the app runs without throwing an exception."""
+        at = AppTest.from_file("streamlit_1_28.py", default_timeout=10).run()
+        assert not at.exception
 
-def test_setting_initial_amount():
-    wallet = Wallet(100)
-    assert wallet.balance == 100
+    def test_sidebar(self):
+        """Test if a single text input for the OpenAI API key exists in the sidebar."""
+        at = AppTest.from_file("streamlit_1_28.py").run()
+        assert len(at.sidebar.text_input) == 1
 
-def test_wallet_add_cash():
-    wallet = Wallet(10)
-    wallet.add_cash(90)
-    assert wallet.balance == 100
+    def test_session_state(self):
+        """Test if the session state is initialized correctly."""
+        at = AppTest.from_file("streamlit_1_28.py").run()
+        assert "messages" in at.session_state
+        assert at.session_state["messages"][0]["role"] == "assistant"
 
-def test_wallet_spend_cash():
-    wallet = Wallet(20)
-    wallet.spend_cash(10)
-    assert wallet.balance == 10
+    def test_messages_render(self):
+        """Test if the initial assistant message is rendered."""
+        at = AppTest.from_file("streamlit_1_28.py").run()
+        assert len(at.chat_message) == 1
 
-def test_wallet_spend_cash_raises_exception_on_insufficient_amount():
-    wallet = Wallet()
-    with pytest.raises(InsufficientAmount):
-        wallet.spend_cash(100)
+    @patch("openai.ChatCompletion.create")
+    def test_openai_api(self, mock_openai):
+        """Test if the OpenAI API is correctly integrated and the chat updates."""
+        mock_openai.return_value = SimpleNamespace(
+            choices=[SimpleNamespace(message=SimpleNamespace(
+                content='Sure, I can help!', role='assistant'))]
+        )
+        at = AppTest.from_file("streamlit_1_28.py").run()
+        at.text_input[0].set_value("test_api_key").run()
+        assert at.session_state["openai_api_key"] == "test_api_key"
+        at.chat_input[0].set_value("Hello").run()
+        assert len(at.session_state["messages"]) == 3
+
+    @patch("openai.ChatCompletion.create")
+    def test_openai_401_error(self, mock_openai):
+        """Test if the app correctly handles a 401 Authentication Error from OpenAI."""
+        mock_openai.side_effect = openai.error.AuthenticationError(
+            "Invalid Authentication")
+        at = AppTest.from_file("streamlit_1_28.py").run()
+        at.text_input[0].set_value("wrong_api_key").run()
+        at.chat_input[0].set_value("Hello").run()
+        if len(at.text_area) > 0:
+            assert "Invalid Authentication" in at.text_area[0].get_value()
+        else:
+            print("text_area is empty! Something's wrong!")
